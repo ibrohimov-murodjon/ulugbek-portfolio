fbf9c8cf33fc35929ba45186941a3320b2c0582a
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-03-10 18:15:03 +0500

    Add list of projects

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,3 +1,60 @@
-const projects = [];
+const projects = [
+  {
+    title: "Tic Tac Toe",
+    description:
+      "Hammamizga ma'lum va mashhur bo'lgan 'Крестики-Нолики' o'yini. Ushbu o'yinni kompyuter bilan yoki do'stingiz bilan o'ynashingiz mumkin. Sizning hamda sizning virtual yoki real do'stingiz to'plagan ballar doimo hisoblab turiladi.",
+    tehnologies: ["JavaScript", "HTML", "Tailwind CSS", "DaisyUI", "ViteJS"],
+    commits: 30,
+    createdDate: "2023-12-1",
+    github: "https://github.com/mirzobox/tic-tac-toe-selfmade",
+    website: "https://tic-tac-toe-selfmade.netlify.app",
+  },
+  {
+    title: "Memory game",
+    description:
+      "3 yoshdan katta bolajonlar uchun xotirani yaxshilashda hamda rivojlantirishda muhim ro'l o'ynaydigan o'yin. Nomidan anglaganingizdek bu xotira o'yini. Saytga tashrif buyurganizda sizni o'yin sozlamalari oynasi kutib oladi. O'yinni o'zingizga moslab sozlang va o'yindan zavq oling.",
+    tehnologies: ["JavaScript", "HTML", "CSS"],
+    commits: 40,
+    createdDate: "2023-10-26",
+    github: "https://github.com/MIRZOULUGBEK9008/memory-game-selfmade",
+    website: "https://memory-game-selfmade.vercel.app",
+  },
+  {
+    title: "Audiophile eccomerce",
+    description:
+      "Qulay hamda sifatli eshitish moslamalarini taklif qiladigan internet do'kon. Ushbu sayt orqali siz ro'yhatdan o'tib, o'zingizga yoqqan quloqchinlar va karnaylarni buyurtma qilishingiz mumkin. Hammasi juda oddiy.",
+    tehnologies: ["React", "React router", "Firebase", "Tailwind CSS"],
+    commits: 20,
+    createdDate: "2024-02-23",
+    github: "https://github.com/mirzobox/audiophile-selfmade",
+    website: "https://audiophile-selfmade.netlify.app",
+  },
+  {
+    title: "Rock, Paper, Scissors",
+    description:
+      "Yoshligimizdan biron o'yin o'ynasak kim birinchi, kim ikkinchi bo'lishini nima orqali aniqlar edik ? Albatta 'Don-don ziki orqali'. Ushbu saytda real hayotdan farqli ravishda sizning sherigingiz kompyuter bo'ladi. Endi yutishingiz biroz qiyinlashadi )",
+    tehnologies: ["JavaScript", "HTML", "Tailwind CSS", "DaisyUI", "ViteJS"],
+    commits: 15,
+    createdDate: "2023-11-29",
+    github: "https://github.com/mirzobox/rock-paper-scissors-selfmade",
+    website: "https://rock-paper-scissors-selfmade.netlify.app",
+  },
+  {
+    title: "Mir Posuda",
+    description:
+      "Farg'ona shahrida yirikroq bo'lgan idish-tovoqlar hamda maishiy texnika bilan shug'ullanadigan firma",
+    tehnologies: [
+      "React",
+      "React router",
+      "TailwindCSS",
+      "Material Tailwind",
+      ".NET",
+    ],
+    commits: 50,
+    createdDate: "2024-1-31",
+    github: "https://github.com/MIRPOSUDA/mir-posuda-frontend",
+    website: "https://mirposuda-frontend.netlify.app",
+  },
+];
 
 export default projects;
